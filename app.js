c6b8c93416fde126db616a10703be79bf03c80c6
esti-winkler-207 <77900987+esti-winkler-207@users.noreply.github.com>
2021-07-08 11:32:10 +0300

    commit4

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const mongoose=require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config()
 const bodyParser = require('body-parser')
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();})
 app.use(bodyParser.json());
+
+
+
+
 
 const connectionParams = {
     useNewUrlParser: true,
@@ -20,4 +33,4 @@ mongoose.connect(process.env.DB_CONNECT, connectionParams).then(() => {
     console.log(err)
 })
 app.use('/',router);
-app.listen(3000,()=>{console.log('listen to port 3000')})
+app.listen(4000,()=>{console.log('listen to port 4000')})
